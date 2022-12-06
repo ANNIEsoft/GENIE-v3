373496b1c28d46464fad8261c050d52b84baf5d8
jminock <jamesminock1997@gmail.com>
2022-12-06 16:11:53 -0500

    define GENIE in proper order

--- annie/Setup_GENIE.sh
+++ annie/Setup_GENIE.sh
@@ -10,6 +10,8 @@ export DISPLAY=:0
 
 export ROOTSYS=${Gapp}/root-6.24.06/install/
 
+export GENIE=${Gapp}/Generator-v3_00_04_ub3-master
+
 export LD_LIBRARY_PATH=/lib:.:${Gapp}/log4cpp/install/lib:${Gapp}/Pythia6Support/v6_424/lib:${ROOTSYS}/lib:${Gapp}/LHAPDF-6.3.0/install/lib:${GENIE}/install/lib:${LD_LIBRARY_PATH}
 
 export PYTHIA6_DIR=${Gapp}/Pythia6Support/v6_424/
@@ -17,8 +19,6 @@ export PYTHIA6_INCLUDE_DIR=${Gapp}/Pythia6Support/v6_424/inc/
 export PYTHIA6_LIBRARY=${Gapp}/Pythia6Support/v6_424/lib/
 
 export LHAPATH=${Gapp}/LHAPDF-6.3.0/install/share/LHAPDF:${LHAPATH}
-
-export GENIE=${Gapp}/Generator-v3_00_04_ub3-master
 
 export PATH=.:${Gapp}/fsplit/:${ROOTSYS}/bin:${Gapp}/LHAPDF-6.3.0/install/bin:${GENIE}/install/bin:${PATH}
 
